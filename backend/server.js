dcd187b6fb949f23f09f5815f0b16ad347fa1920
Stalin Patel <stalinpatel023@gmail.com>
2025-04-29 20:04:08 +0530

    fixed path conflict in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,38 @@
-import express from "express";
-import "dotenv/config";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import "dotenv/config";
+import path from "path";
+import express from "express";
+import { connectDB } from "./lib/db.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import authRoutes from "./routes/auth.route.js";
-import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
-import paymentRoutes from "./routes/payment.route.js";
-import analyticsRoutes from "./routes/analytics.route.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import paymentRoutes from "./routes/payment.route.js";
+import productRoutes from "./routes/product.route.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); //allows you to parse the body of the request
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes); // DONE
-app.use("/api/products", productRoutes); // DONE 1left - productRecommendation
-app.use("/api/cart", cartRoutes); // DONE
-app.use("/api/coupons", couponRoutes); // DONE
-app.use("/api/payments", paymentRoutes); // DONE
-app.use("/api/analytics", analyticsRoutes); // DONE
-app.use("/api/orders", orderRoutes); // DONE
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/orders", orderRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(5000, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   connectDB();
 });
-
-// TODO - ADD SORT FUNCTION IN COUPONS
-// 1- SORT BY EXPIRY DATE
-// 2- SORT BY DATE CREATED
-
-// TOMORROW TODO-
-// START WITH RAZORPAY INTEGRATION
-//ADD ADDRESSES
-//AFTER PAYMENT UPDATE THE COUPON USAGE DETAILS IN THE DATABASE TO PREVENT MULTIPLE TIME USES
